3f0544e6b03814ffef3d30d53678794fc6b3c743
Brian LeQuang <brian.lequang@gmail.com>
2020-04-30 18:22:44 -0500

    init glfw, glew, and glm

---
+++ npr/src/main.cpp
@@ -0,0 +1,57 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#include "glm/glm.hpp"
+#include "glm/gtx/string_cast.hpp"
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "glewInit failed" << std::endl;
+    }
+
+    // Testing glm
+    glm::mat4 projection_matrix_ = glm::mat4(1.0);
+    std::cout << glm::to_string(projection_matrix_) << std::endl;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
