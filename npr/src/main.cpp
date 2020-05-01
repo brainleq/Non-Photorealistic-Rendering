384662e0ff2568b31464a07c9d3db521d826276b
Brian LeQuang <brian.lequang@gmail.com>
2020-05-01 18:58:35 -0500

    learning how to set up shaders

--- npr/src/main.cpp
+++ npr/src/main.cpp
@@ -6,6 +6,31 @@
 #include "glm/glm.hpp"
 #include "glm/gtx/string_cast.hpp"
 
+static unsigned int compile_shader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(GL_VERTEX_SHADER);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    // TODO: error handling
+
+    return id;
+}
+
+static int create_shader(const std::string& vertex_shader, const std::string& fragment_shader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compile_shader(GL_VERTEX_SHADER, vertex_shader);
+    unsigned int fs = compile_shader(GL_FRAGMENT_SHADER, fragment_shader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);         // TODO: detach shader after linking?
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -33,17 +58,34 @@ int main(void)
     glm::mat4 projection_matrix_ = glm::mat4(1.0);
     std::cout << glm::to_string(projection_matrix_) << std::endl;
 
+    float positions[] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6, positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        /*glBegin(GL_TRIANGLES);
         glVertex2f(-0.5f, -0.5f);
         glVertex2f(0.0f, 0.5f);
         glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glEnd();*/
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
